76708e150a179e944efacb3b5a9a26f5c8158c60
bu4kovski <buckovskislavco@gmail.com>
2023-01-03 13:18:50 +0100

    upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import LifecycleFuncComponent from "./components/LifecycleFuncComponent";
+import LifecycleClassComponent from "./components/LifecycleClassComponent";
+import { LogIn } from "./components/LogIn"
+
+const App = () => {
+  return (<div>
+    <LogIn />
+    </div> )
+
+  }
+
+
+  // {/* <LifecycleFuncComponent />
+  // <LifecycleClassComponent /> */}
+
+  // </>);
+// }
+export default App;
